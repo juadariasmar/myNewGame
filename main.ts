847e203d473ef04a39901b157abf0212aa0439fa
Juan  Arias <45865805+juadariasmar@users.noreply.github.com>
2021-09-22 10:07:07 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,89 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairWest, function (sprite, location) {
+    game.over(true, effects.blizzard)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+	
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorLight3, function (sprite, location) {
+    info.changeLifeBy(-1)
+    tiles.placeOnRandomTile(Heroe, sprites.dungeon.stairLadder)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    Enemigo.destroy(effects.fire, 500)
+})
+let Enemigo: Sprite = null
+let Heroe: Sprite = null
+Heroe = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 8 8 f f f . . . . 
+    . . . f f f 8 8 8 8 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 8 8 8 8 8 8 e e f . . 
+    . . f e 8 f f f f f f 8 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 8 8 8 8 8 8 f 4 e . . 
+    . . 4 d f 8 8 8 8 8 8 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let Tesoro = sprites.create(img`
+    . . . . . . . b b . . . . . . . 
+    . . . . . . b d d b . . . . . . 
+    . . . . . b d 5 5 d b . . . . . 
+    . . . . b b 5 5 5 5 b b . . . . 
+    . . . . b 5 5 5 5 5 5 b . . . . 
+    b b b b b 5 5 5 5 1 1 d b b b b 
+    b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b 
+    b d d 5 5 5 5 5 5 1 1 1 5 d d b 
+    . b d d 5 5 5 5 5 5 5 5 d d b . 
+    . . b b 5 5 5 5 5 5 5 5 b b . . 
+    . . c b 5 5 5 5 5 5 5 5 b c . . 
+    . . c 5 5 5 5 d d 5 5 5 5 c . . 
+    . . c 5 5 d b b b b d 5 5 c . . 
+    . . c 5 d b c c c c b d 5 c . . 
+    . . c c c c . . . . c c c c . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+controller.moveSprite(Heroe, 100, 100)
+info.setScore(0)
+info.setLife(3)
+info.startCountdown(15)
+scene.cameraFollowSprite(Heroe)
+tiles.setTilemap(tilemap`level1`)
+tiles.placeOnRandomTile(Heroe, sprites.dungeon.stairLadder)
+tiles.placeOnRandomTile(Tesoro, sprites.dungeon.floorLight2)
+game.onUpdateInterval(5000, function () {
+    Enemigo = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f11111111f.......
+        ......fd11111111df......
+        ......fd11111111df......
+        ......fddd1111dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf11fcdcf......
+        .......fb111111ffff.....
+        ......fffcdb1bc111cf....
+        ....fc111cbfbf1b1b1f....
+        ....f1b1b1ffffbfbfbf....
+        ....fbfbfffffff.........
+        .........fffff..........
+        ..........fff...........
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(Enemigo, sprites.dungeon.floorLight2)
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`100010000102020c02020203020202020c020203050a0a0a0a0a0a040a0a0a0a0a0a0a040b0a0a0a0e0a0a040a0a0a0a0e0a0a0c050a0a0a0a0a0a040a0a0a0a0a0a0a04050a0a0a0a0a0a040a0a0a0707070708050a0a07070707080a0e0a0a0a0a0a04050a0a0a0a0a0a090a0a0a0a0a0a0a04050a0e0a0a0e0a090a0a0a0a0a0a0a0c0c0a0a0a0a0a0a090a0a0a0707070708050a0a0a0a0a0a0a0a0a0a0a0a0a0a04050a0a0a0a090a0a0a0a040a0a0a0a04050a0a0a0a090a0a0e0a040a0a0e0a0c050a0e0a0a090a0a0a0a040a0a0a0a040c0a0a0a0a090a0e0a0a040a0a0a0a04050a0a0a0a090a0a0a0a040a0a0a0a04060707070708070707070807070d0708`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . 2 . . . . . . . 2 
+. . . . . . . 2 . . . . . . . 2 
+2 . . . . . . 2 . . . . . . . 2 
+2 . . . . . . 2 . . . 2 2 2 2 2 
+2 . . 2 2 2 2 2 . . . . . . . 2 
+2 . . . . . . 2 . . . . . . . 2 
+2 . . . . . . 2 . . . . . . . 2 
+2 . . . . . . 2 . . . 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . 2 . . . . 2 . . . . 2 
+2 . . . . 2 . . . . 2 . . . . 2 
+2 . . . . 2 . . . . 2 . . . . 2 
+2 . . . . 2 . . . . 2 . . . . 2 
+2 . . . . 2 . . . . 2 . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 . 2 2 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorLight2,sprites.dungeon.stairWest,sprites.dungeon.greenOuterNorth2,sprites.dungeon.stairLadder,sprites.dungeon.floorLight3], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
